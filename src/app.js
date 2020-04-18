b8142bdc8ad3bad43e4bdea617fe6881d028ae08
breakfastmeansbreakfast <77744c@gmail.com>
2020-04-18 20:40:00 +0100

    finished strings

--- src/app.js
+++ src/app.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const libstrings = require('./lib/strings');
 
 const app = express();
 
 app.get('/strings/hello/:string', (req, res) => {
   res.status(200).json({ result: `Hello, ${req.params.string}!` });
+});
+
+app.get('/strings/upper/:string', (req, res) => {
+  res.status(200).json({ result: libstrings.uppercase(req.params.string) });
+});
+
+app.get('/strings/lower/:string', (req, res) => {
+  res.status(200).json({ result: libstrings.lowercase(req.params.string) });
+});
+
+app.get('/strings/first-characters/:string', (req, res) => {
+  if (isNaN(req.query.length)) {
+    res.status(200).json({ result: libstrings.firstCharacter(req.params.string) });
+  } else {
+    res
+      .status(200)
+      .json({ result: libstrings.firstCharacters(req.params.string, req.query.length) });
+  }
 });
 
 module.exports = app;
